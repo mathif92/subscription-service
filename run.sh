458c4003be005afee27cb5020b6ff03b3cfa2b6e
mathif92 <mathif925@gmail.com>
2019-01-24 22:40:02 -0300

    Fixed container name

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ else
     docker run --name subscription-mysql -p 3308:3306 -e MYSQL_ROOT_PASSWORD=secret-pass -e MYSQL_USER=subscription -e MYSQL_PASSWORD=subscription -e MYSQL_DATABASE=subscription -d mysql:latest
 fi
 
-if [ "$(docker ps -a | grep subscription-mysql)" ]; then
+if [ "$(docker ps -a | grep subscription-service)" ]; then
     docker start subscription-service
 else
     # Generate the application's jar file
